cff2bb972149463ea87099c1705381833503f708
Dembereldodov <adembereldodov1210@gmail.com>
2024-04-28 22:57:10 +0800

    aa

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -8,7 +8,7 @@ import { environment } from 'src/environments/environment';
 })
 export class UserService {
     // Change the apiUrl to a relative path that matches your proxy configuration
-    private apiUrl = environment.apiUrl + '/users';
+    private apiUrl = environment.apiUrl + '/users/';
 
     constructor(private http: HttpClient) {
         console.log('apiUrl: ', this.apiUrl);
